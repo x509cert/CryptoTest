60dcfff0e04a79eb4b833b0e50d6cb29c3171657
mikehow <mikehow@vm-firstam-cryp>
2021-07-15 18:02:37 +0000

    Add project files.

---
+++ CryptoTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Data.Encryption.Cryptography;
+using Microsoft.Data.Encryption.Cryptography.Serializers;
+
+// Use this VM's Managed Service Identity
+var creds = new DefaultAzureCredential();
+
+// Get the AKV URL from the VM metadata
+var kvUri = await SupportKeyVault.GetKeyVaultUrl();
+
+// Create an AKV client
+var client = new SecretClient(new Uri(kvUri), creds);
+
+// Pull out the secret used for the key
+string secretName = "BaseKey"; 
+KeyVaultSecret secret = client.GetSecret(secretName);
+
+// Build a symmetric key
+var key = System.Convert.FromBase64String(secret.Value);
+PlaintextDataEncryptionKey encryptionKey = new("BaseKey", key);
+
+// Crypto options and parameters
+var encryptionSettings = new EncryptionSettings<string>(
+    dataEncryptionKey: encryptionKey,
+    encryptionType: EncryptionType.Deterministic,
+    serializer: StandardSerializerFactory.Default.GetDefaultSerializer<string>()
+);
+
+// Now that the crypto is setup, time to connect to SQL using the VM creds
+
+
+string plaintextString = "This is a secret message";
+var ciph = plaintextString.Encrypt(encryptionSettings);
+var plain = ciph.Decrypt<string>(encryptionSettings);
+Console.Write(plain);
---
+++ CryptoTest/Support-SQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Data.SqlClient;
+using Azure.Identity;
+using Azure.Core;
+
+/// <summary>
+/// This class supports database access from the VM
+/// https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/tutorial-windows-vm-access-sql
+/// </summary>
+
+public class SupportSQL
+{
+    public static void Connect(DefaultAzureCredential creds)
+    {
+        // Get the OAuth2 token to Azure SQL
+        var oauth2TokenSql = creds.GetToken(
+                new TokenRequestContext(
+                    new[] { "https://database.windows.net/.default" })).Token;
+
+        string connectionString = "Data Source=<AZURE-SQL-SERVERNAME>; Initial Catalog=<DATABASE>;";
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.AccessToken = oauth2TokenSql;
+        conn.Open();
+
+    }
+}
