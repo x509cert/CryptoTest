5fdc4def0d6adcbe1b81afe5d4cb7a419b7bcfea
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2021-07-17 03:06:34 +0000

    Simple version, File I/O only

--- CryptoTest/Program.cs
+++ CryptoTest/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.IO;
-using System.Data;
-using Microsoft.Data.SqlClient;
+using System.Text;
 using Azure.Identity;
 using Microsoft.Data.Encryption.Cryptography;
 using Microsoft.Data.Encryption.Cryptography.Serializers;
-using Azure.Core;
-using Microsoft.Data.Encryption.AzureKeyVaultProvider;
+using Azure.Security.KeyVault.Secrets;
+
+#region File I/O setup
+if (args.Length !=1)
+{
+    Console.WriteLine("Usage: CryptoTest {e|d}");
+    return;
+}
+
+bool encrypt = (args[0] == "e") ? true : false;
+string fileIn = encrypt ? @"c:\lotr\lotr.csv" : @"c:\lotr\lotr-enc.csv";
+string fileOut = encrypt ? @"c:\lotr\lotr-enc.csv" : @"c:\lotr\lotr.csv";
+
+#endregion
 
 #region Setup (Azure login, AKV and crypto settings)
+
 // Use this VM's Managed Service Identity
 var creds = new DefaultAzureCredential();
 
-// Get the AKV URL from the VM metadata
-var kvUri = await SupportKeyVault.GetKeyVaultUrl();
+// setup a connection to AKV
+var keyVaultUrl = @"https://kv-cryptotest.vault.azure.net/";
+var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: creds);
 
-// Get the key from AKV
-EncryptionKeyStoreProvider azureKeyProvider = new AzureKeyVaultKeyStoreProvider(creds);
-KeyEncryptionKey keyEncryptionKey = new KeyEncryptionKey("KEK", kvUri, azureKeyProvider);
-ProtectedDataEncryptionKey encryptionKey = new ProtectedDataEncryptionKey("DEK", keyEncryptionKey);
+KeyVaultSecret secret = client.GetSecret("CryptoKey");
+var key = System.Convert.FromBase64String(secret.Value);
+PlaintextDataEncryptionKey encryptionKey = new("DEK", key);
 
 // Crypto options and parameters
 var encryptionSettings = new EncryptionSettings<string>(
@@ -29,74 +41,31 @@ var encryptionSettings = new EncryptionSettings<string>(
 
 #endregion
 
-#region Read from CSV File and built up the DataTable
-// read all entries from the CSV file, and encrypt the last element (SSN)
-var records = File.ReadAllLines(@"c:\\lotr\\lotr.csv");
-DataTable dt = new DataTable();
+#region Read from CSV File, encrypt and save the CSV file
 
-for (int i = 0; i < records.Length; i++)
+// read all entries from the CSV file, and encrypt the last element (SSN)
+var recordsIn = File.ReadAllLines(fileIn);
+var temp = new StringBuilder();
+
+for (int i = 0; i < recordsIn.Length; i++)
 {
-    string[] elem = records[i].Split(',');
+    string[] elem = recordsIn[i].Split(',');
     if (elem.Length <= 1) break;
 
-    if (i == 0) //headers
+    //headers
+    if (i == 0) 
     {
-        dt.Columns.Add(elem[0], typeof(string));
-        dt.Columns.Add(elem[1], typeof(string));
-        dt.Columns.Add(elem[2], typeof(Byte[]));
+        temp.AppendLine(recordsIn[i]);
     }
+    // data
     else
     {
-        dt.Rows.Add();
-        dt.Rows[i - 1].SetField("name", elem[0]);
-        dt.Rows[i - 1].SetField("location", elem[1]);
-        dt.Rows[i - 1].SetField("ssn", elem[2].Encrypt(encryptionSettings));
+        temp.AppendLine(elem[0] + "," + elem[1] + "," + 
+            (encrypt ? Convert.ToBase64String(elem[2].Encrypt(encryptionSettings))
+                     : Convert.FromBase64String(elem[2]).Decrypt<string>(encryptionSettings)));
     }
 }
 
-dt.AcceptChanges();
+File.WriteAllText(fileOut, temp.ToString());
 
 #endregion
-
-#region SQL
-
-var connectionString = "Data Source=sql-cryptotest.database.windows.net; Initial Catalog=LoTR;Column Encryption Setting=enabled";
-using (var connection = new SqlConnection(connectionString))
-{
-    var token = creds.GetToken(
-            new TokenRequestContext(
-                new[] { "https://database.windows.net/.default" })).Token;
-
-    connection.AccessToken = token;
-
-    connection.Open();
-
-    using (var bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.AllowEncryptedValueModifications, null))
-    {
-        string[] dbColumns = { "name", "location", "ssn" };
-
-        foreach (var column in dbColumns)
-            bulkCopy.ColumnMappings.Add(column, column);
-
-        bulkCopy.DestinationTableName = "[dbo].[Peoples2]";
-
-        bulkCopy.WriteToServer(dt);
-    }
-}
-
-
-//// Connect to Azure SQL using the VM creds
-//var sql = new SupportSQL(creds);
-//sql.Connect();
-//sql.BulkCopy(encryptedRecords.ToString());
-
-
-
-//sql.Close();
-
-#endregion
-
-//string plaintextString = "This is a secret message";
-//var ciph = plaintextString.Encrypt(encryptionSettings);
-//var plain = ciph.Decrypt<string>(encryptionSettings);
-//Console.Write(plain);
